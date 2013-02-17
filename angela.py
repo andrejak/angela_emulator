4c8e2f15123dae2099fb4674a3c49e2147d59e7c
Andreja Kogovsek <andreja.kogovsek1@gmail.com>
2013-02-17 22:33:18 +0100

    If you answer with an adjective, it works.

--- angela.py
+++ angela.py
@@ -3,15 +3,30 @@
 
 import random
 
-# Start talking to Angela
-print("Angela: 'What's up?'")
-response = raw_input("You: ")
+angela = "Angela: "
+
+questions = [
+  "How was your day?"
+]
 
 insults = [
   "Your face is ", 
   "Your mum is ", 
   "Your mum wishes she was ",
-  "Your mum wishes her face was "]
+  "Your mum wishes her face was "
+]
+
+index = random.randint(0, len(questions) - 1)
+
+# Start talking to Angela
+print(angela + questions[index])
+response = raw_input("You: ")
+
+# Extract sentence parts (nouns, adjectives, maybe later verbs)
+
+# adjective:          X -> X
+# nouns:              X -> a/an? X
+# nouns to adjective: X -> (more) like a X
 
 # Extract the part to use as the insult
-print("Angela: " + insults[random.randint(0, 3)] + response)
+print(angela + insults[random.randint(0, len(insults) - 1)] + response)
