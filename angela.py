cd9adab628489ece65877259f7c0e7deeaddde2c
Andreja Kogovsek <andreja.kogovsek1@gmail.com>
2013-02-18 09:25:56 +0100

    Another question added.

--- angela.py
+++ angela.py
@@ -6,6 +6,7 @@ import random
 angela = "Angela: "
 
 questions = [
+  "What do you think of Ruth?",
   "How was your day?"
 ]
 
@@ -13,20 +14,23 @@ insults = [
   "Your face is ", 
   "Your mum is ", 
   "Your mum wishes she was ",
-  "Your mum wishes her face was "
+  "Your mum wishes her face was ",
+  "Your mum wishes your face was "
 ]
 
-index = random.randint(0, len(questions) - 1)
-
 # Start talking to Angela
-print(angela + questions[index])
+q = random.randint(0, len(questions) - 1)
+print(angela + questions[q])
 response = raw_input("You: ")
 
 # Extract sentence parts (nouns, adjectives, maybe later verbs)
 
-# adjective:          X -> X
+# adjective:          X -> X (negative positive ones later?)
 # nouns:              X -> a/an? X
 # nouns to adjective: X -> (more) like a X
 
 # Extract the part to use as the insult
-print(angela + insults[random.randint(0, len(insults) - 1)] + response)
+if (q == 0):
+  print(angela + "Yeah, I agree.")
+else:
+  print(angela + insults[random.randint(0, len(insults) - 1)] + response + ".")
