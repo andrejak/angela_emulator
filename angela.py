1143424e0d1541deac23d4b72c85b828eb1ea5f9
Andreja Kogovsek <andreja.kogovsek1@gmail.com>
2013-02-18 13:42:18 +0100

    Added questions etc and randomization so answers are different every time. Moved a few things around.

--- angela.py
+++ angela.py
@@ -4,48 +4,62 @@
 import random
 import nltk
 
+# Constants
+
 angela = "Angela: "
+was = "was "
+could = "could "
+fail = "Your mum's face wishes her face's mum was... I mean... Wait..."
+angela_broken = "Well done. You broke Angela. I hope you're happy now."
 
 questions = [
-  "What do you think of Ruth?",
-  "How was your day?"
+  "What do you think about Ruth?",
+  "How was your day?",
+  "How are you doing?",
+  "What have you been up to?",
+  "What do you think about me?",
+  "Where's Rebecca?",
+  "When was the last time you saw Dave?",
+  "What's the best thing about cats?",
+  "Do you know what Andreja's plotting now?"
+]
+
+insults_wishes = [
+  "Your mum wishes she ",
+  "Your mum wishes her face ",
+  "Your mum wishes your face "
 ]
 
 insults = [
   "Your face is ", 
-  "Your mum is ", 
-  "Your mum wishes she was ",
-  "Your mum wishes her face was ",
-  "Your mum wishes your face was "
-]
+  "Your mum is "
+] + [wish + was for wish in insults_wishes]
 
-# Start talking to Angela
-q = random.randint(0, len(questions) - 1)
-print(angela + questions[q])
-response = raw_input("You: ")
+# Helper function definitions
 
-# Extract sentence parts (nouns, adjectives, maybe later verbs)
-
-# adjective:          X -> X (negative positive ones later?)
-# nouns:              X -> a/an? X
-# nouns to adjective: X -> (more) like a X
-
-tokens = nltk.word_tokenize(response)
-print tokens
-tagged = nltk.pos_tag(tokens)
-print tagged
-
-# Adjective: JJ, JJR (comparative), JJS (superlative)
-# Adverb:    RB, RBR (comparative), RBS (suplerative)
-# Noun:      NN, NNS (plural), NNP (proper), NNPS (proper plural)
-# Verbs:     VB (base), VBD (past), VBZ (3rd person singular present) ...
+def random_member(from_list):
+  return from_list[random.randint(0, len(from_list) - 1)]
 
 def tokens_by_tag(tagged_tokens, search_tag):
   return [token for (token, tag) in tagged_tokens if tag == search_tag]
 
+# Start talking to Angela
+question = random_member(questions)
+print angela +question 
+response = raw_input("You: ")
+
+# Extract sentence parts (nouns, adjectives, maybe later verbs)
+tokens = nltk.word_tokenize(response)
+tagged = nltk.pos_tag(tokens)
+
+# http://www.monlp.com/2011/11/08/part-of-speech-tags/
+# Adjective: JJ, JJR (comparative), JJS (superlative)
+# Adverb:    RB, RBR (comparative), RBS (superlative)
+# Noun:      NN, NNS (plural), NNP (proper), NNPS (proper plural)
+# Verbs:     VB (base), VBD (past), VBZ (3rd person singular present) ...
 adjectives             = tokens_by_tag(tagged, "JJ")
 adjectives_comparative = tokens_by_tag(tagged, "JJR")
-adverbs_superlative    = tokens_by_tag(tagged, "RBS")
+adjectives_superlative = tokens_by_tag(tagged, "JJS")
 adverbs                = tokens_by_tag(tagged, "RB")
 adverbs_comparative    = tokens_by_tag(tagged, "RBR")
 adverbs_superlative    = tokens_by_tag(tagged, "RBS")
@@ -54,9 +68,21 @@ nouns_plural           = tokens_by_tag(tagged, "NNS")
 verbs                  = tokens_by_tag(tagged, "VB")
 verbs_past             = tokens_by_tag(tagged, "VBD")
 verbs_3rd              = tokens_by_tag(tagged, "VBZ")
+#print tagged # DEBUG
 
 # Extract the part to use as the insult
-if (q == 0):
-  print(angela + "Yeah, I agree.")
+def generate_all_possible_repartees():
+  repartees = []
+  repartees += [angela + random_member(insults) + adjective + "." for adjective in adjectives + adjectives_comparative]
+  repartees += [angela + random_member(insults) + "the " + adjective + "!" for adjective in adjectives_superlative + adverbs_superlative + nouns]
+  repartees += [angela + random_member(insults_wishes) + could + verb + "." for verb in verbs]
+  if repartees:
+    return repartees
+  return [angela + fail + "\n" + angela_broken]
+  #return [angela + random_member(insults) + response + "."]
+
+# Generate an insult as a response
+if ("Ruth" in question):
+  print angela + "Yeah, I agree."
 else:
-  print(angela + insults[random.randint(0, len(insults) - 1)] + response + ".")
+  print random_member(generate_all_possible_repartees())
